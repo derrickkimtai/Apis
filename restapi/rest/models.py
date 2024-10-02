f1d955583e5e11b0067790b32c49f363cba5fde1
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-02 14:20:06 +0300

    added a model name

--- restapi/rest/models.py
+++ restapi/rest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Name(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
