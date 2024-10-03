0eed7f8ee7ebc64cfccaea0192f413f3df8cbfe1
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-03 12:41:32 +0300

    creatig a custo user sign up using drf

--- restapi/rest/models.py
+++ restapi/rest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Name(models.Model):
@@ -8,3 +9,14 @@ class Name(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    password2 = models.CharField(max_length=100)
--- restapi/rest/serializers.py
+++ restapi/rest/serializers.py
@@ -1,7 +1,38 @@
 from rest_framework import serializers
 from .models import Name
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 class NameSeralizer(serializers.ModelSerializer):
     class Meta:
         model = Name
         fields = '__all__'
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'middle_name', 'last_name', 'phone')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"Password entered does not match "})
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            middle_name=validated_data['middle_name'],
+            last_name=validated_data['last_name'],
+            phone=validated_data['phone'],
+        )
+
+        user.set_password(validated_data['password']) #hash the password
+        user.save()
+
+        return user
