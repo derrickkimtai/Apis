f6582303e249455cf111487f06cc34b20aa18732
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-05 15:16:06 +0300

    added a signup for pasword2

--- restapi/rest/serializers.py
+++ restapi/rest/serializers.py
@@ -13,11 +13,25 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'phone']
+        fields = ['username', 'email', 'password', 'password2',  'phone']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match"})
+        return attrs
+    
 
     def create(self, validated_data):
+
+        validated_data.pop('password2')
+
         user = CustomUser(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
